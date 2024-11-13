c1f2394dc2050eead155b5224fcc30be4740355c
Maxime Dussort <maxime.dussort@gmail.com>
2024-11-13 11:46:20 +0100

    Working moving system without level interaction

--- src/scripts/Player.py
+++ src/scripts/Player.py
@@ -1,19 +1,33 @@
 from random import randint
+# from importations import level
 import numpy as np
 
 class Player:
-    keyboard = {
-        "up": "z",
-        "left": "q",
-        "down": "s",
-        "right": "d"
-        }
 
     def __init__(self):
+        self.keyboard = {
+            "up": "z",
+            "left": "q",
+            "down": "s",
+            "right": "d"
+        }
         self.locatedAt = np.array([1, 1])
+        # self.level = level()
 
-    def move(self, direction: str):
+    def move(self):
+        nextTileCoords = self.getDirection()
+        
+        self.locatedAt = nextTileCoords
+        return self.locatedAt
+
+    def getDirection(self):
+        direction = "none"
         newLocation = self.locatedAt
+        actionInput = input("Action (Z, Q, S, D, ...):")
+        if actionInput in self.keyboard.values() :
+            direction = list(self.keyboard.keys())[list(self.keyboard.values()).index(actionInput)]
+        else:
+            print("I didn't understood")
         if direction == "up" :
             newLocation[1] = newLocation[1]-1
         elif direction == "down" :
@@ -22,21 +36,9 @@ class Player:
             newLocation[0] = newLocation[0]-1
         elif direction == "right" :
             newLocation[0] = newLocation[0]+1
-        else:
-            print("I didn't understood")
-        self.locatedAt = newLocation
-        return self.locatedAt
+        return newLocation
 
-    def getDirection(self):
-        """Permet de demander et renvoie en conséquence la direction"""
-        pass
-
-
-# thePlayer = Player()
-
-# thePlayer.move("down")
-# thePlayer.move("down")
-# thePlayer.move("right")
-# thePlayer.move("down")
-
-# print(thePlayer.locatedAt)
+thePlayer = Player()
+while True:
+    thePlayer.move()
+    print(thePlayer.locatedAt)
